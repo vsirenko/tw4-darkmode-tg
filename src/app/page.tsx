c95414c872608c186d664dd44a542535d4f70449
vsirenko <the.sirenko@gmail.com>
2024-07-31 19:07:48 +0300

    chore: init, dark selector for class

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className=" bg-amber-400 dark:bg-amber-800 w-screen h-screen flex items-center justify-center">
+      <h1 className=" text-5xl text-white">HELLO WORLD</h1>
+    </main>
+  );
+}
